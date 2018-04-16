931ab73d206a728d9e6f3b29b7dba2984378bf85
ciker_killer <2050459191@qq.com>
2018-04-17 01:20:04 +0800

    update websocket

--- src/main/java/com/tt/websocket/handler/WebSocketEndPoint.java
+++ src/main/java/com/tt/websocket/handler/WebSocketEndPoint.java
@@ -1,17 +1,33 @@
 package com.tt.websocket.handler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 public class WebSocketEndPoint extends TextWebSocketHandler{
-
+	private static Logger log=LoggerFactory.getLogger(WebSocketEndPoint.class);
 	@Override
 	protected void handleTextMessage(WebSocketSession session,
 			TextMessage message) throws Exception {
 		super.handleTextMessage(session, message);
 		TextMessage returnMessage = new TextMessage(message.getPayload()+" received at server");  
-        session.sendMessage(returnMessage);  
+		log.info("handleTextMessage....");
+		session.sendMessage(returnMessage);  
+	}
+
+	@Override
+	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+		log.info("afterConnectionClosed....");
+		super.afterConnectionClosed(session, status);
+	}
+
+	@Override
+	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+		log.info("afterConnectionEstablished....");
+		super.afterConnectionEstablished(session);
 	}
 
 	
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,29 @@
-<html>
-<body>
-<h2>Hello World!</h2>
-</body>
-</html>
+<!DOCTYPE html>  
+<html>  
+<head>  
+  
+</head>  
+<script type="text/javascript" src="https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js"></script>
+<body>  
+<script type="text/javascript">
+  $(function() {
+    console.log("abc");
+    $.ajax({url:"http://localhost:8000/threeTalk/socket/login/1",success:function(result){
+      console.log(result);
+      var ws = new WebSocket("ws://localhost:8000/threeTalk/myHandler")
+       ws.onopen = function () {
+        console.log("onpen");
+       ws.send("{}");
+       }
+       ws.onclose = function () {
+        console.log("onclose");
+       }
+
+      ws.onmessage = function (msg) {
+        console.log(msg.data);
+       }
+    }});
+  })
+</script>
+</body>  
+</html>  
